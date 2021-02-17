ae82bbb52ab37a9909b27c470ea50ded2c0c4db6
Kristoffer <ktis10@hotmail.com>
2021-02-17 13:22:03 +0100

    Vi har tilføjet intens på, så vi kan gå mellem layouts. Samt lavet Startside og resultat til relativt layouts

--- app/src/main/java/grp5/cdio/solitairesolver/StartLoadingScreen.java
+++ app/src/main/java/grp5/cdio/solitairesolver/StartLoadingScreen.java
@@ -2,7 +2,9 @@ package grp5.cdio.solitairesolver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class StartLoadingScreen extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class StartLoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_loading_screen);
+
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                // if you are redirecting from a fragment then use getActivity() as the context.
+                startActivity(new Intent(StartLoadingScreen.this, StartSide.class));
+
+            }
+        };
+
+
+        Handler h = new Handler();
+
+        // The Runnable will be executed after the given delay time
+        h.postDelayed(r, 5000); // will be delayed for 3 seconds
+
     }
 }
