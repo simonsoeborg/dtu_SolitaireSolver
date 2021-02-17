d6db4e257d0dc24a3ea4552afa76243862d632f4
Kristoffer <ktis10@hotmail.com>
2021-02-17 12:24:22 +0100

    Vi har lavet de 3 klasser vi skal bruge og er igang med layouts. Layout for StartLoadingScreen er done. Nu skal vi have appen til at start på den

---
+++ app/src/main/java/grp5/cdio/solitairesolver/StartLoadingScreen.java
@@ -0,0 +1,14 @@
+package grp5.cdio.solitairesolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartLoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_loading_screen);
+    }
+}
