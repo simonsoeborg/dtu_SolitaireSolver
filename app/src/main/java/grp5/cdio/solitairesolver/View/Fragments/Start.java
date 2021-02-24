55e980da9ad01ec381f546a8a4d619f72fe07186
Simon Søborg <s173255@student.dtu.dk>
2021-02-24 11:30:16 +0100

    Huge changes made. Refactor, Fragments and re-structure

---
+++ app/src/main/java/grp5/cdio/solitairesolver/View/Fragments/Start.java
@@ -0,0 +1,42 @@
+package grp5.cdio.solitairesolver.View.Fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import grp5.cdio.solitairesolver.R;
+import grp5.cdio.solitairesolver.View.StartLoadingScreen;
+
+
+public class Start extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View startFrag = inflater.inflate(R.layout.fragment_start, container, false);
+
+        Button tagBillede = startFrag.findViewById(R.id.TagBilledeBtn_startFrag);
+        Context context = StartLoadingScreen.context;
+
+        tagBillede.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, Resultat.class));
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.FragmentFL, new Resultat())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        return startFrag;
+    }
+}
