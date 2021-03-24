fa700438eca2750fc0242f57dca32eaf35ab21c1
SimonF <s195479@student.dtu.dk>
2021-03-24 10:44:34 +0100

    Additional piles in Table

--- app/src/main/java/grp5/cdio/solitairesolver/Model/Table.java
+++ app/src/main/java/grp5/cdio/solitairesolver/Model/Table.java
@@ -3,9 +3,10 @@ package grp5.cdio.solitairesolver.Model;
 import java.util.ArrayList;
 
 public class Table {
-    private ArrayList<Pile> groundPile;
-    private ArrayList<Pile> buildPile;
-
+    private ArrayList<Pile> groundPile; // The four "columns in the right corner.
+    private ArrayList<Pile> buildPile; // The seven columns in the middle of the board.
+    private ArrayList<Pile> drawPile; // The draw pile.
+    private ArrayList<Pile> discardPile; // Pile of already drawn cards.
 
     @Override
     public String toString(){
