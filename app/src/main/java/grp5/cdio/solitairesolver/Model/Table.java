f6fb7d408820cb8c5dad098e9d771b3991b7eb8a
SimonF <s195479@student.dtu.dk>
2021-03-24 11:10:49 +0100

    Konstructor outline

--- app/src/main/java/grp5/cdio/solitairesolver/Model/Table.java
+++ app/src/main/java/grp5/cdio/solitairesolver/Model/Table.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 public class Table {
     private ArrayList<Pile> groundPile; // The four "columns in the right corner.
     private ArrayList<Pile> buildPile; // The seven columns in the middle of the board.
-    private ArrayList<Pile> drawPile; // The draw pile.
-    private ArrayList<Pile> discardPile; // Pile of already drawn cards.
+    private Pile drawPile; // The draw pile.
+    private Pile discardPile; // Pile of already drawn cards.
 
     @Override
     public String toString(){
         return null;
     }
+
+    public Table(){
+
+    }
+
+
 }
