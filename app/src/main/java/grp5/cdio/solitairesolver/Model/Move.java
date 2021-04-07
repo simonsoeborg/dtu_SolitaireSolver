6c12cd9b8c6f689904ab9baaec76208d263a8a0c
ElinorMohr <ElinorMohr@users.noreply.github.com>
2021-04-07 09:46:54 +0200

    Move start

--- app/src/main/java/grp5/cdio/solitairesolver/Model/Move.java
+++ app/src/main/java/grp5/cdio/solitairesolver/Model/Move.java
@@ -3,14 +3,26 @@ package grp5.cdio.solitairesolver.Model;
 public class Move {
     private Card moveFrom;
     private Card moveTo;
+    private Card card;
     private int score;
+
+    public Move(Card from, Card to, int score, Card card){
+        moveFrom = from;
+        moveTo = to;
+        this.score = score;
+        this.card = card;
+    }
 
     @Override
     public String toString(){
-        return null;
+
+        return "Move "+card+" from " + moveFrom.toString() + " to " + moveTo.toString();
     }
 
-    public boolean isInverseMove(){
+    public boolean isInverseMove(Move oldMove){
+        if (oldMove.moveFrom.isEqual(moveTo) && oldMove.moveTo.isEqual(moveFrom) && oldMove.card.isEqual(card)){
+            return true;
+        }
         return false;
     }
 
---
+++ app/src/test/java/grp5/cdio/solitairesolver/MoveTest.java
@@ -0,0 +1,28 @@
+package grp5.cdio.solitairesolver;
+
+import grp5.cdio.solitairesolver.Model.Card;
+import grp5.cdio.solitairesolver.Model.FaceValue;
+import grp5.cdio.solitairesolver.Model.Move;
+import grp5.cdio.solitairesolver.Model.Suit;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MoveTest {
+    @Test
+    public void addition_isCorrect() {
+
+        Card cardQueenHearts = new Card(Suit.HEARTS, FaceValue.TWELVE);
+        Card cardKingClubs = new Card(Suit.CLUBS, FaceValue.THIRTEEN);
+        Card cardKingSpades = new Card(Suit.SPADES, FaceValue.THIRTEEN);
+
+
+        //Move move = new Move();
+        Move newMove = new Move(cardKingClubs,cardKingSpades, 0, cardQueenHearts);
+        Move oldMove = new Move(cardKingSpades, cardKingClubs, 0, cardQueenHearts);
+
+
+        //assertTrue(oldMove.isInverseMove(newMove));
+    }
+}
