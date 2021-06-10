9944fdcd8d4cb788beadf3a774c9f37cb69eee27
ElinorMohr <ElinorMohr@users.noreply.github.com>
2021-06-10 09:19:00 +0200

    Notes

--- app/src/androidTest/java/grp5/cdio/solitairesolver/ObjectDetectionTest.java
+++ app/src/androidTest/java/grp5/cdio/solitairesolver/ObjectDetectionTest.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import grp5.cdio.solitairesolver.Controller.ObjectDetection.ObjectDetection;
 import grp5.cdio.solitairesolver.Controller.ObjectDetection.Result;
@@ -38,6 +39,19 @@ public class ObjectDetectionTest {
         BufferedInputStream bufferedInputStream = new BufferedInputStream(is);
         Bitmap bit = BitmapFactory.decodeStream(bufferedInputStream);
         ArrayList<Result> resultArrayList = objectDetection.analyzeBitmap(bit);
+
+        // top left - lav left giver mest left maks 2 instanser af hvert kort
+        // bottom right
+
+        //prøv evt at teste med andre billeder
+        // cluster metode i java?
+
+        // Fundet alle med samme id
+        // lave linje med top left, evt byg buffer løbende(Se på hvad der er af afstand pt fra de andres billeder)
+        // Dannet kort basseret på ID
+        // lagt kort i matchende bunke fra top left
+
+
         Float totalScore = new Float(0);
         for(Result result : resultArrayList){
             totalScore = totalScore + result.getScore();
